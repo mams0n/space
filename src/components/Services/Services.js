df9c68723ebdd5add2a6dd3777268207636ec4da
Mamuka Mamulashvili <m.mamulashvili111@gmail.com>
2021-02-19 20:47:26 +0400

    Final

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -84,23 +84,6 @@ const Services = () => {
         >
           <span className="middle_planet">Mobile App</span>
         </div>
-        {/* <div className="random_planet1"></div>
-        <div className="random_planet2"></div>
-        <div className="random_planet3"></div>
-        <div className="random_planet4"></div>
-        <div className="random_planet5"></div>
-        <div className="random_planet6"></div>
-        <div className="random_planet7"></div>
-        <div className="random_planet8"></div>
-        <div className="random_planet9"></div>
-        <div className="random_planet10"></div>
-        <div className="random_planet11"></div>
-        <div className="random_planet12"></div>
-        <div className="random_planet13"></div>
-        <div className="random_planet14"></div>
-        <div className="random_planet15"></div>
-        <div className="random_planet16"></div>
-        <div className="random_planet17"></div> */}
         <Background />
       </div>
     </div>
